0969e6fd6096065e6ae5a38c182952c8c5ca3387
Noora Vainio <nooraevainio@gmail.com>
2017-03-22 19:54:53 +0200

    add second sort key

--- ratkaisin.py
+++ ratkaisin.py
@@ -1,6 +1,7 @@
 # Simple hangman solver, guesses letters in order of frequency
 import re
 import sys
+import logging
 print('noora')
 
 MAX_MISSES = 8
@@ -10,7 +11,7 @@ words = []
 
 word = input().strip()
 while word:
-    word = word.encode('latin-1').decode('utf-8')
+    #word = word.encode('latin-1').decode('utf-8')
     words.append(word)
     word = input().strip()
 
@@ -18,7 +19,7 @@ original_words = words
 
 try:
     status = input()
-    status = status.encode('latin-1').decode('utf-8')
+    #status = status.encode('latin-1').decode('utf-8')
     while status:
         index = 0
         words = original_words
@@ -28,30 +29,32 @@ try:
         words = [word for word in words if len(word) == len(status)]
         guessed_letters = []
         letters = {letter for word in words for letter in word}
-        frequencies = [(letter, sum(word.count(letter) for word in words)) for letter in letters]
+        frequencies = [(letter, sum(word.count(letter) for word in words),  len([word for word in words if letter in word])) for letter in letters]
+        guess_order = sorted(frequencies, key=lambda a: (a[1], -a[1]), reverse=True)
+        #frequencies = [(letter, len([word for word in words if letter in word])) for letter in letters]
         guess_order = sorted(frequencies, key=lambda a: a[1], reverse=True)
         while True:
             word_count = len(words)
 
             # if three or less words left, guess that
             if len(words) <= 3 and len(words) <= guesses_left:
-                sys.stdout.buffer.write((words[0] + '\n').encode('utf-8'))
+                print(words[0])
             # else guess next letter
             else:
                 letter = guess_order[index][0]
                 index += 1
-                sys.stdout.buffer.write((letter + '\n').encode('utf-8'))
+                print(letter)
                 guessed_letters.append(letter)
-            
+
             result = input()
-            result = result.encode('latin-1').decode('utf-8')
+            #result = result.encode('latin-1').decode('utf-8')
             status = input()
-            status = status.encode('latin-1').decode('utf-8')
+            #status = status.encode('latin-1').decode('utf-8')
 
             # when winning or losing and starting a new game prints three lines
             if status.startswith('WIN') or status.startswith('LOSE') or not status:
                 status = input()
-                status = status.encode('latin-1').decode('utf-8')
+                #status = status.encode('latin-1').decode('utf-8')
                 break
 
             # if hit, filter words with correct regex
@@ -84,9 +87,8 @@ try:
             letters = {letter for word in words for letter in word if letter not in guessed_letters}
             # count new frequencies if the number of words has changed
             if len(words) < word_count:
-                frequencies = [(letter, sum(word.count(letter) for word in words)) for letter in letters]
-                guess_order = sorted(frequencies, key=lambda a: a[1], reverse=True)
-
+                frequencies = [(letter, sum(word.count(letter) for word in words),  len([word for word in words if letter in word])) for letter in letters]
+                guess_order = sorted(frequencies, key=lambda a: (a[1], -a[1]), reverse=True)
                 # start from the beginning of the new guess_order
                 index = 0
 
